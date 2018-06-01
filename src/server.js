47d18c6c9d1ec7e38b3c0b4633740e9cb60de714
Larson <rlrson@uiowa.edu>
2018-06-01 16:23:06 -0500

    Add initial server and updated config files to run.

---
+++ src/server.js
@@ -0,0 +1,58 @@
+/**
+* Server for the College of Public Health Event Application
+*/
+/* Dependencies -------------------------------------------------------------*/
+var express     = require('express');
+var mongoose    = require('mongoose');
+var bodyParser  = require('body-parser');
+var getSecret   = require('./secrets');
+
+/* Instances  ---------------------------------------------------------------*/
+const app     = express();
+const router  = express.Router();
+
+/* Configurations -----------------------------------------------------------*/
+// Express
+const PORT = process.env.PORT || 3001;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Mongoose
+var Event = require('./api/models/eventModel');
+mongoose.connect(getSecret('mongodb'), () => { console.log('connected to MongoDB!'); });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
+
+// Routes and stuff
+
+
+// Set Route path & initialize API
+router
+  .get('/', (request, response) => {
+    response.json({message: 'Hello, world!'});
+  })
+  .post('/', (request, response) => {
+    //if (!request.body) response.statusCode(500);
+
+    console.log(request.body);
+
+    //response.statusCode(200);
+    response.end();
+  });
+
+router.post('/api/events', (request, response) => {
+  // Body Parser lets us use JSON. Extract the request information from body
+  const { name, date, time, comments, email } = request.body;
+  
+  // Create a new event, the created/updated/approved fields are pre filled.
+  const event = new Event();
+  event.eventID = new mongoose.mongo.ObjectId();
+});
+
+
+
+// Use router config when we call /api
+app.use('/api', router);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
