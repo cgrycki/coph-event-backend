70ec629fe3fdfdce30cfa78cb361fc36cd660487
Larson <rlrson@uiowa.edu>
2018-07-23 15:05:52 -0500

    Add rooms directory

---
+++ rooms/room.model.js
@@ -0,0 +1,80 @@
+/**
+ * Room Model for DynamoDB.
+ * The room is a critical object for the application, much of
+ * what the user wants revolves around doing it *somewhere*. 
+ * The object is currently a near clone of the MAUI API with 
+ * floors added. 
+ * 
+ * To Do:
+ *  - Slim down attributes! 
+ * Assign semantic meaning to departments
+ * Align rooms reservable status with kelly
+ */
+
+/* DEPENDENCIES -------------------------------------------------------------*/
+// Set up Dynamo to connect, even in our Lambda env.
+var dynamo = require('dynamodb');
+dynamo.AWS.config.update({ region: process.env.AWS_DEFAULT_REGION });
+
+// Database model validation
+const Joi    = require('joi');
+
+// Utility to create our database name
+const createTableName = require('../utils/index').createTableName;
+const name            = process.env.APP_NAME;
+const env             = process.env.EENV;
+const table           = 'rooms';
+
+
+/* MODEL --------------------------------------------------------------------*/
+var Room = dynamo.define('Room', {
+  hashKey: 'roomNumber',
+  schema: {
+    buildingName   : Joi.string().required(),
+    buildingCode   : Joi.string().required(),
+    roomNumber     : Joi.string().required(),
+    roomName       : Joi.string().allow(null).required(),
+    regionList     : Joi.array(),
+    featureList    : Joi.array(),
+    maxOccupancy   : Joi.number().integer(),
+    rmType         : Joi.string().allow(null).required(),
+    acadOrgUnitName: Joi.number().integer().allow(null),
+    roomCategory   : Joi.string().allow(null),
+    roomTypeGroup  : Joi.string().allow(null).required(),
+    floor          : Joi.number().integer().required()
+  },
+  tableName: createTableName(name, env, table)
+});
+
+// RESTful functions
+
+/**
+ * Model function to return a list of rooms as {roomNumber, floor} objects.
+ * @param {any} request Express incoming HTTP request.
+ * @param {any} response Express outgoing HTTP response.
+ */
+Room.getRooms = function(request, response) {
+  Room
+    .scan()
+    .attributes(['roomNumber', 'floor'])
+    .exec((err, data) => {
+      if (err) response.send(404).json(err);
+      else response.status(200).json(data.Items);
+    });
+}
+
+Room.getRoom = function(request, response) {
+  // Gather params from request. Verified by the middleware previous.
+  let roomNumber = request.params.roomNumber;
+
+  Room
+    .query(roomNumber)
+    .limit(1)
+    .exec((err, data) => {
+      if (err) response.status(404).json(err);
+      else response.status(200).json(data.Items);
+    });
+}
+
+
+module.exports = Room;
---
+++ rooms/room.routes.js
@@ -0,0 +1,51 @@
+/**
+ * Room Router
+ * Responsible for returning rooms/schedules from our connected services.
+ */
+
+
+/* Dependencies -------------------------------------------------------------*/
+const express = require('express');
+const router  = express.Router();
+const request = require('request');
+
+const utils     = require('../utils/index');
+const roomUtils = require('./room.utils');
+const Room      = require('./room.model');
+
+
+/* Parameters? ---*/
+router.param('roomNumber',  roomUtils.validRoomNum);
+router.param('date',        roomUtils.validDate);
+router.param('startTime',   roomUtils.validStartTime);
+router.param('endTime',     roomUtils.validEndTime);
+
+
+/* REST ---------------------------------------------------------------------*/
+/* GET /rooms -- List CoPH rooms as JS objects. */
+router.get('/', (req, res) => Room.getRooms(req, res));
+
+/* GET /rooms/:roomNumber -- Get one room's info. */
+router.get('/:roomNumber', utils.validateParams, (req, res) => Room.getRoom(req, res));
+
+
+
+
+
+
+/* GET /rooms/:roomNumber/:date/:startTime-:endTime -- Check if a room is free. */
+//http://localhost:3001/rooms/N110/2017-02-07/
+/*
+router.get('/:roomNumber/:date', //:startTime-:endTime', 
+  [
+    utils.validateParams,
+    roomUtils.addDayMiddleware,
+    roomUtils.getAstraRoomSchedule,
+    roomUtils.formatAstraRoomSchedule
+  ],
+  (request, response) => response.send(request.roomSchedule)
+);
+*/
+
+
+module.exports = router;
