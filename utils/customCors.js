72f5b1975370bd6e33705fb619efe1469baeaa9f
Larson <rlrson@uiowa.edu>
2018-07-23 15:01:59 -0500

    Add utils/ to import

---
+++ utils/customCors.js
@@ -0,0 +1,35 @@
+/**
+ * Custom CORS. 
+ * 
+ */
+
+// List of domains we will accept
+const whitelist_domains = [process.env.REDIRECT_URI, process.env.FRONTEND_URI, 'uiowa.edu'];
+
+// List of Headers we will accept
+const whitelist_headers = [
+  'Authorization', 'Content-Type', 'Content-type', 'x-api-key',
+  'Origin', 'origin', 'X-Amz-Date', 'X-Requested-With', 'x-requested-with', 
+  'Access-Control-Request-Headers', 'Access-Control-Request-Method', 
+  'Access-Control-Request-Origin', 'Access-Control-Request-Credentials',
+  'Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials',
+  'access-control-allow-origin', 'access-control-allow-credentials',
+  'Cache-Control', 'cache-control'
+];
+
+// Whitelist of HTTP Methods
+const whitelist_methods = 'GET,PUT,POST,DELETE,PATCH,OPTIONS';
+
+// CORS options for npm's CORS package
+const cors_options = {
+  credentials   : true,
+  origin        : whitelist_domains,
+  methods       : whitelist_methods,
+  allowedHeaders: whitelist_headers
+};
+
+
+exports.whitelist_domains = whitelist_domains;
+exports.whitelist_headers = whitelist_headers;
+exports.whitelist_methods = whitelist_methods;
+exports.cors_options      = cors_options;
---
+++ utils/xray.js
@@ -0,0 +1,46 @@
+/**
+ * AWS Xray tracing middleware
+ */
+
+// Dependecies
+var xrayAWS = require('aws-xray-sdk');
+var xrayExpress = require('aws-xray-sdk-express');
+
+
+// App Name
+var app_name = 'workflow-test' || process.env.APP_NAME +'-'+ process.env.EENV;
+
+
+// Used as middleware before the routes are assigned.
+var startTrace = xrayAWS.express.openSegment(app_name);
+
+
+const requestTrace = (request, response, next) => {
+  xrayAWS.captureAsyncFunc('send', function(subsegment) {
+    // Capture AWS Credentials
+    subsegment.addAnnotation("AWS_ACCESS_KEY_ID", `${process.env.AWS_ACCESS_KEY_ID}`);
+    subsegment.addAnnotation("AWS_SECRET_ACCESS_KEY", `${process.env.AWS_SECRET_ACCESS_KEY}`);
+    subsegment.addAnnotation("AWS_SESSION_TOKEN", `${process.env.AWS_SESSION_TOKEN}`);
+    subsegment.addAnnotation("AWS_REGION", `${process.env.AWS_REGION}`);
+
+    // Capture cookies and session information
+    subsegment.addAnnotation("REQUEST_COOKIES", JSON.stringify(request.cookies));
+    subsegment.addAnnotation("REQUEST_SESSION", JSON.stringify(request.session));
+
+    // Capture request/response headers
+    subsegment.addAnnotation("REQUEST_HEADERS", JSON.stringify(request.headers));
+    subsegment.addAnnotation("RESPONSE_HEADERS", JSON.stringify(response.headers));
+
+    // Close as instructed by AWS
+    subsegment.close();
+  });
+  next();
+};
+
+// Make sure to close the xray after the routes are done! :: app.use(xrayAWS.express.closeSegment());
+const endTrace = xrayAWS.express.closeSegment();
+
+
+exports.startTrace   = startTrace;
+exports.requestTrace = requestTrace;
+exports.endTrace     = endTrace;
