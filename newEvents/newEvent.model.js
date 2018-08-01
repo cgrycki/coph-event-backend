68abae1c135b71eac2f751cbc43c3a52ff00a8fc
Larson <rlrson@uiowa.edu>
2018-08-01 17:54:26 -0500

    Remove conditionals from dynamo model, may add seperate validation.

--- newEvents/newEvent.model.js
+++ newEvents/newEvent.model.js
@@ -25,44 +25,32 @@ const EventModel = dynamo.define('Event', {
 
   schema: {
     // Workflow and DynamoDB primary key
-    package_id       : Joi.number().required(),
+    package_id    : Joi.number().required(),
 
     // Contact information
-    user_email       : Joi.string().email().regex(/uiowa\.edu$/).required(),
-    contact_email    : Joi.string().optional().allow("").email(),
-    coph_email       : Joi.string().optional().allow("").email().regex(/uiowa\.edu$/),
+    user_email    : Joi.string().email().regex(/uiowa\.edu$/).required(),
+    contact_email : Joi.string().optional().allow("").email(),
+    coph_email    : Joi.string().optional().allow("").email().regex(/uiowa\.edu$/),
 
     // Event Information
-    event_name       : Joi.string().min(3).max(75).trim().required(),
-    comments         : Joi.string().trim().max(3000).allow("").default(""),
-    date             : Joi.date().iso().required(),
-    start_time       : Joi.string().trim().min(7).max(8).required(),
-    end_time         : Joi.string().trim().min(7).max(8).required(),
-    room_number      : Joi.string().alphanum().max(10).required(),
-    num_people       : Joi.number().min(1).max(206).required().default(1),
+    event_name    : Joi.string().min(3).max(75).trim().required(),
+    comments      : Joi.string().trim().allow("").max(3000).required(),
+    date          : Joi.date().iso().required(),
+    start_time    : Joi.string().trim().min(7).max(8).required(),
+    end_time      : Joi.string().trim().min(7).max(8).required(),
+    room_number   : Joi.string().alphanum().max(10).required(),
+    num_people    : Joi.number().min(1).max(206).required().default(1),
 
     // Auxillary information
-    references_course: Joi.boolean().required().default(false),
-    referenced_course: Joi.string().allow("").default("").when("references_course", {
-        is  : true,
-        then: Joi.string().required()
-    }),
+    references_course   : Joi.boolean().required().default(false),
+    referenced_course   : Joi.string().allow("").required(),
 
-    setup_required: Joi.boolean().required().default(false),
-    setup_mfk     : Joi.string().alphanum().allow("").when("setup_required", {
-      is  : true,
-      then: Joi.string().alphanum().required()
-    }),
+    setup_required      : Joi.boolean().required().default(false),
+    setup_mfk           : Joi.string().alphanum().allow("").required(),
 
-    food_drink_required: Joi.boolean().required().default(false),
-    food_provider      : Joi.string().trim().allow("").when("food_drink_required", {
-      is  : true,
-      then: Joi.string().min(5)
-    }),
-    alcohol_provider   : Joi.string().trim().allow("").when("food_drink_required", {
-      is  : true,
-      then: Joi.string().min(5)
-    })
+    food_drink_required : Joi.boolean().required().default(false),
+    food_provider       : Joi.string().trim().allow("").required(),
+    alcohol_provider    : Joi.string().trim().allow("").required()
   },
 
   // Dynamic table names depending on our Node environment
