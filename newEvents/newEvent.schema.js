c1201051c95309ac9bb31b1cb0aa4c31df64db64
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-08-02 15:57:36 -0500

    Add more tests!

--- newEvents/newEvent.schema.js
+++ newEvents/newEvent.schema.js
@@ -5,56 +5,94 @@
 const Joi = require('joi');
 
 /* BASE --------------------------------------------------------------------*/
-const string = Joi.string().allow("");
-const email = Joi.string().allow("").email();
-const date = Joi.date().iso().required();
-const time = Joi.string().regex(/^(1[0-2]|0?[1-9]):([0-5]?[0-9])(●?[AP]M)?$/).required();
-
-
-
+const jString = Joi.string();
+const jBool   = Joi.boolean().required();
+const email   = jString.allow("").email();
+const date    = Joi.date().iso().required();
+const time    = jString.regex(/^(1[0-2]|0?[1-9]):([0-5]?[0-9])(●?[AP]M)?$/).required();
 
 
 /* SCHEMA + CASES ------------------------------------------------------------*/
+const package_id = Joi.number().required();
+
 const user_email = email.regex(/uiowa\.edu$/).required();
+
 const contact_email = email;
+
 const coph_email = email.regex(/uiowa\.edu$/);
 
+const event_name = jString.min(5).max(75).trim().required();
 
+const comments = jString.allow("").max(3000).required();
 
+const room_number = jString.alphanum().max(10).required();
 
-/* CASES --------------------------------------------------------------------*/
-const test_cases = {
-  user_email: [
-    null,
-    undefined,
-    "",
-    "test@gmail",
-    "test@gmail.com",
-    "test@uiowa.edu"
-  ],
-  contact_email: [
-    null,
-    undefined,
-    "",
-    "event@planner",
-    "event@planner.com",
-    "event@uiowa.edu"
-  ],
-  coph_email: [
-    null,
-    undefined,
-    "",
-    "test@gmail",
-    "test@gmail.com",
-    "test@uiowa.edu"
-  ]
+const num_people = Joi.number().min(1).max(206).required();
+
+const course = Joi.object().keys({
+  references_course: Joi.boolean().required(),
+  referenced_course: jString
+    .when('references_course', {
+      is  : false,
+      then: jString.allow("").required()
+    })
+    .when('references_course', {
+      is  : true,
+      then: jString.min(5).required()
+    })
+});
+
+const setup = Joi.object().keys({
+  setup_required: jBool,
+  setup_mfk     : jString
+    .when('setup_required', {
+      is  : false,
+      then: jString.allow("").required()
+    })
+    .when('setup_required', {
+      is  : true,
+      then: jString.required()
+    })
+});
+
+const ModelSchema = {
+  package_id: package_id,
+
+  // Contact information
+  user_email   : user_email,
+  contact_email: contact_email,
+  coph_email   : coph_email,
+
+  // Event information
+  event_name : event_name,
+  comments   : comments,
+  date       : date,
+  start_time : time,
+  end_time   : time,
+  room_number: room_number,
+  num_people : num_people,
+
+  // Auxillary information
+  references_course   : jBool,
+  referenced_course   : jString.allow("").required(),
+
+  setup_required      : jBool,
+  setup_mfk           : jString.allow("").required(),
+
+  food_drink_required : jBool,
+  food_provider       : jString.allow("").required(),
+  alcohol_provider    : jString.allow("").required()
 };
 
 
-
-
-
-
-exports.user_email = user_email;
+exports.package_id    = package_id;
+exports.user_email    = user_email;
 exports.contact_email = contact_email;
-exports.coph_email = coph_email;
+exports.coph_email    = coph_email;
+exports.date          = date;
+exports.time          = time;
+exports.comments      = comments;
+exports.num_people    = num_people;
+exports.course        = course;
+exports.setup         = setup;
+exports.ModelSchema   = ModelSchema;
--- test/newEvent.spec.js
+++ test/newEvent.spec.js
@@ -22,10 +22,10 @@ const runJoiTest = (description, value, schema, trueValue) => {
 };
 
 
-describe('Event Schema', function() {
+describe('Event Schema (Individual)', function() {
 
   // User email: Required, U Iowa address
-  describe('#user_email_schema()', function() {
+  describe('#user_email', function() {
     let scheme = Schema.user_email;
 
     runJoiTest('Should not validate a null value.',         {user_email: null}, scheme, null);
@@ -36,7 +36,7 @@ describe('Event Schema', function() {
   });
 
   // Contact email: Not required, email
-  describe('#contact_email_schema()', function() {
+  describe('#contact_email', function() {
     let scheme = Schema.contact_email;
 
     runJoiTest('Should not validate a null value.',  {contact_email: null}, scheme, null);
@@ -45,13 +45,127 @@ describe('Event Schema', function() {
     runJoiTest('Should allow an U Iowa email.',      {contact_email: "test@uiowa.edu"}, scheme, Error);
   });
 
-  // CoPH email
-  describe('#coph_email_schema()', function() {
+  // CoPH email: Optional, but must be U. Iowa address
+  describe('#coph_email', function() {
     let scheme = Schema.coph_email;
 
     runJoiTest('Should not validate a null value.',     {coph_email: null}, scheme, null);
     runJoiTest('Should allow an empty string.',         {coph_email: ""}, scheme, Error);
     runJoiTest('Should not allow an NON U Iowa email.', {coph_email: "test@gmail.com"}, scheme, null);
     runJoiTest('Should allow a U Iowa email.',          {coph_email: "test@uiowa.edu"}, scheme, Error);
+  });
+
+  // Date: Required, must be YYYY-MM-DD format.
+  describe('#Date', function() {
+    let scheme = Schema.date;
+
+    runJoiTest('Should not validate a null value.', {date: null}, scheme, null);
+    runJoiTest('Should not allow an empty string.', {date: ""}, scheme, null);
+    runJoiTest('Should not allow malformed values', {date: "Aug 1st, 2018"}, scheme, null);
+    runJoiTest('Should allow YYYY-MM-DD values.',   {date: "2018-08-01"}, scheme, Error);
+  });
+
+  // Time: Required, in H:MM A format
+  describe('#Time', function() {
+    let scheme = Schema.time;
+
+    runJoiTest('Should not validate a null value.', {time: null}, scheme, null);
+    runJoiTest('Should allow an empty string.',     {time: ""}, scheme, Error);
+    runJoiTest('Should not allow HH:MM format.',    {time: "23:00"}, scheme, null);
+    runJoiTest('Should allow H:MM A format (with two hour digits).',       {time: "12:30 PM"}, scheme, Error);
+    runJoiTest('Should allow H:MM A format (with one hour digits).',       {time: "8:30 PM"}, scheme, Error);
+    runJoiTest('Should allow H:MM A format (with AM).',       {time: "12:30 AM"}, scheme, Error);
+    runJoiTest('Should allow H:MM A format (with PM).',       {time: "10:30 PM"}, scheme, Error);
+  });
+
+  // Course reference
+  describe('#Course References', function() {
+    let scheme = Schema.course;
+
+    runJoiTest(
+      'Should not allow empty values for both course values',
+      {references_course: null, referenced_course: null},
+      scheme, null);
+    runJoiTest(
+      'Should not allow empty values for references_course',
+      {references_course: null, referenced_course: ""},
+      scheme, null);
+    runJoiTest(
+      'Should not allow empty values for referenced_course',
+      {references_course: false, referenced_course: null},
+      scheme, null);
+    runJoiTest(
+      'Should allow empty string when references_course is false',
+      {references_course: false, referenced_course: ""},
+      scheme, Error);
+    runJoiTest(
+      'Should NOT allow empty string when references_course is true',
+      {references_course: true, referenced_course: ""},
+      scheme, null);
+    runJoiTest(
+      'Should allow populated string when references_course is true',
+      {references_course: false, referenced_course: "Introduction to Public Health"},
+      scheme, Error);
+  });
+
+  // Setup
+  describe('#Setup', function() {
+    let scheme = Schema.setup;
+
+    runJoiTest(
+      'Should not allow empty values for both course values',
+      {setup_required: null, setup_mfk: null},
+      scheme, null);
+    runJoiTest(
+      'Should not allow empty values for setup_required',
+      {setup_required: null, setup_mfk: ""},
+      scheme, null);
+    runJoiTest(
+      'Should not allow empty values for setup_mfk',
+      {setup_required: false, setup_mfk: null},
+      scheme, null);
+    runJoiTest(
+      'Should allow empty string when setup_required is false',
+      {setup_required: false, setup_mfk: ""},
+      scheme, Error);
+    runJoiTest(
+      'Should NOT allow empty string when setup_required is true',
+      {setup_required: true, setup_mfk: ""},
+      scheme, null);
+    runJoiTest(
+      'Should allow populated string when setup_required is true',
+      {setup_required: true, setup_mfk: "1234567890"},
+      scheme, Error);
+  });
+});
+
+describe('Event Schema (Total)', function() {
+  let schema = Schema.ModelSchema;
+
+  describe('#Minimum viable schema', function() {
+    let info = {
+      user_email: 'test@uiowa.edu',
+      contact_email: '',
+      coph_email: '',
+      event_name: 'Curing Cancer',
+      comments: '',
+      date: '2018-08-01',
+      start_time: '8:00 AM',
+      end_time: '12:00 PM',
+      room_number: 'XC100',
+      num_people: 1,
+      references_course: false,
+      referenced_course: '',
+      setup_required: false,
+      setup_mfk: '',
+      food_drink_required: false,
+      food_provider: '',
+      alcohol_provider: ''
+    };
+
+    runJoiTest(
+      'Should validate a minimum viable object',
+      info,
+      schema, Error);
   });
 });
