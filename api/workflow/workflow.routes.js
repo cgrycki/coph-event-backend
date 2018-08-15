cc42d987d8df33c5505ffe0d70009b2bc4de481e
Larson <rlrson@uiowa.edu>
2018-08-14 20:36:44 -0500

    Include async before pushing.

--- api/workflow/workflow.routes.js
+++ api/workflow/workflow.routes.js
@@ -38,7 +38,7 @@ router.get('/inbox', (request, response) => {
 
 // DELETE package
 router.delete('/', [checkSessionExists, retrieveSessionInfo],
-  (req, res) => {
+  async (req, res) => {
     // Gather params for calling RESTful Workflow endpoint
     const { uiowa_access_token, user_ip_address } = req;
     const { package_id } = req.body;
