7e40423530ee0a50df7083c7f83b77987823ecc3
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-08-15 17:35:21 -0500

    Fix temp typo

--- api/auth/auth.routes.js
+++ api/auth/auth.routes.js
@@ -10,7 +10,7 @@ const {
   checkSessionExistsMiddleware,
   retrieveSessionInfoMiddleware,
   clearTokensFromSessionMiddleware
-}            = require('./auth.middleware');
+}            = require('./auth.utils');
 
 
 /* Parameters ---------------------------------------------------------------*/
