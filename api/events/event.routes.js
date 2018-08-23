3852bf72fdac46c7dcfd2eb144c7aefcb0c90d11
Larson <rlrson@uiowa.edu>
2018-08-22 19:11:56 -0500

    Fix middleware typo

--- api/events/event.routes.js
+++ api/events/event.routes.js
@@ -47,7 +47,7 @@ router.get('/my', getDynamoEventsMiddleware,
   (req, res) => res.status(200).json(req.evts));
 
 // TESTING
-router.get('/my/test', getDynamoEventMiddleware, (req, res) => {
+router.get('/my/test', getDynamoEventsMiddleware, (req, res) => {
 
   const { evts } = req;
   // Map each event to it's packageID
