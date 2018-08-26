846665713f0fb22fcb86671f21de01bf2230835e
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-08-25 19:15:19 -0500

    Implement new route for schedules

--- api/maui/maui.routes.js
+++ api/maui/maui.routes.js
@@ -1,7 +1,7 @@
 /**
- * MAUI Router
- * Responsible for returning rooms, schedules, and courses from our connected services.
- */
+* MAUI Router
+* Responsible for returning rooms, schedules, and courses from our connected services.
+*/
 
 
 /* Dependencies -------------------------------------------------------------*/
@@ -14,18 +14,25 @@ const {
   validStartDate,
   validEndDate,
   getRoomScheduleMiddleware,
+  newGetRoomSchedulesMiddleware,
   getCoursesMiddleware
 }                         = require('./maui.utils');
 
 
 /* Parameters ---------------------------------------------------------------*/
-router.param('room_number', validRoomNum);
-router.param('date',        validDate);
-router.param('startDate',   validStartDate);
-router.param('endDate',     validEndDate);
+router.param('room_number',  validRoomNum);
+router.param('date',         validDate);
+router.param('start_date',   validStartDate);
+router.param('end_date',     validEndDate);
 
 
 /* REST ---------------------------------------------------------------------*/
+// TESTING
+router.get("/", (req, res) => {
+  console.log(req.query);
+  res.json(req.query);
+});
+
 
 /* GET /rooms -- List CoPH rooms as JS objects. */
 router.get('/rooms', (req, res) => Room.getRooms(req, res));
@@ -36,12 +43,17 @@ router.get('/rooms/:room_number', validateParams, (req, res) => Room.getRoom(req
 
 
 /* GET /rooms/:room_number/:date -- Get Astra Schedule for a room */
-router.get('/rooms/:room_number/:date', 
-  validateParams, getRoomScheduleMiddleware,
+router.get('/rooms/:room_number/:date', validateParams, getRoomScheduleMiddleware,
   (req, res) => res.status(200).json({
-      message: `${req.events.length} events found`,
-      events: req.events
-    }));
+    message: `${req.events.length} events found`,
+    events: req.events
+  }));
+
+
+/* GET /schedules/:YYYY-MM-DD/:2018-08-01/?room_number='N110'&... */
+router.get('/schedules/:start_date/:end_date/', 
+  validRoomNum, validateParams, newGetRoomSchedulesMiddleware,
+  (req, res) => res.status(200).json({ schedule: req.schedule }));
 
 
 router.get('/courses/:courseText', getCoursesMiddleware,
