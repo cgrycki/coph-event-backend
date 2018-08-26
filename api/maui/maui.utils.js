c00fd308ded76a8c411cf65123424e39ad7b33e7
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-08-25 19:14:53 -0500

    Add middleware and alter parameter validation for new schedule function

--- api/maui/maui.utils.js
+++ api/maui/maui.utils.js
@@ -4,32 +4,27 @@
 
 
 /* Dependencies -------------------------------------------------------------*/
-const { check } = require('express-validator/check');
-const MAUI      = require('./MAUI');
+const { check }   = require('express-validator/check');
+const MAUI        = require('./MAUI');
 
 
 /* Paramater Validation -----------------------------------------------------*/
-
-// roomId
 const validRoomNum = check('room_number')
-  .exists().withMessage('Must have a roomId to access API')
-  .isString().withMessage('roomId must be a string')
-  .isAlphanumeric().withMessage('roomId must be alphanumeric')
+  .exists().withMessage('Must have a room_number to access API')
+  .isAlphanumeric().withMessage('room_number must be alphanumeric')
   .isUppercase().withMessage('Must be uppercased')
   .trim();
-
-// date
 const validDate = check('date')
   .exists().withMessage('You need a date param')
   .isString().withMessage('Date should be a date-coercable string')
   .isISO8601().withMessage('Date param should be formatted YYYY-mm-dd')
   .trim();
-const validStartDate = check('startDate')
+const validStartDate = check('start_date')
   .exists().withMessage('You need a date param')
   .isString().withMessage('Date should be a date-coercable string')
   .isISO8601().withMessage('Date param should be formatted YYYY-mm-dd')
   .trim();
-const validEndDate = check('endDate')
+const validEndDate = check('end_date')
   .exists().withMessage('You need a date param')
   .isString().withMessage('Date should be a date-coercable string')
   .isISO8601().withMessage('Date param should be formatted YYYY-mm-dd')
@@ -52,6 +47,33 @@ async function getRoomScheduleMiddleware(request, response, next) {
     request.events = (result !== undefined) ? result : [];
     next();
   };
+}
+
+
+/**
+ * Middleware mapping Express HTTP requests to MAUI REST call for room(s) schedules. Function assumes parameters and room_number have been validated by prior middlewares. 
+ * @param {Object} request Incoming HTTP request object
+ * @param {Object} response Outgoing HTTP response
+ * @param {Object} next Next function in route. (callback is after this function)
+ */
+async function newGetRoomSchedulesMiddleware(request, response, next) {
+  // Gather function inputs for MAUI request
+  const start_date = request.params.start_date,
+        end_date   = request.params.end_date,
+        rooms = (typeof(request.query.room_number) === 'string') ? 
+            [request.query.room_number]  : request.query.room_number;
+
+  // Make and eval REST call.
+  const schedule = await MAUI.getSchedules(rooms, start_date, end_date);
+  if (schedule.error) return response.status(400).json({
+    error  : true,
+    message: schedule.message,
+    stack  : schedule.stack
+  });
+
+  // Otherwise pass it along
+  request.schedule = schedule;
+  return next();
 }
 
 
@@ -80,5 +102,6 @@ module.exports = {
   validStartDate,
   validEndDate,
   getRoomScheduleMiddleware,
+  newGetRoomSchedulesMiddleware,
   getCoursesMiddleware
 };
