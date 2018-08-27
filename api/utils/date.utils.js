f1701fc9ef06dc6264685ee26b929119ab28f250
Larson <rlrson@uiowa.edu>
2018-08-27 17:54:44 -0500

    Try returning ISO time for frontend w/ localizer.

--- api/utils/date.utils.js
+++ api/utils/date.utils.js
@@ -13,7 +13,7 @@ const getFormattedDateTime = (date=new Date(), time="4:50PM") => {
   const format = 'MMMM DD, YYYY hh:mmA';
 
   // Format it and output to local datetime
-  const formattedDateTime = moment(dateTime, format).local().toLocaleString();
+  const formattedDateTime = moment(dateTime, format).local().toISOString();
   return formattedDateTime;
 }
 
