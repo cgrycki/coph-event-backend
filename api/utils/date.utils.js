f047e1b00d5923237a53bb6fb1f09f3ef11fd06f
Larson <rlrson@uiowa.edu>
2018-08-27 18:23:05 -0500

    ANother moment parsing update...

--- api/utils/date.utils.js
+++ api/utils/date.utils.js
@@ -13,7 +13,7 @@ const getFormattedDateTime = (date=new Date(), time="4:50PM") => {
   const format = 'MMMM DD, YYYY hh:mmA';
 
   // Format it and output to local datetime
-  const formattedDateTime = moment(dateTime, format).format();
+  const formattedDateTime = moment(dateTime, format).local().format();
   return formattedDateTime;
 }
 
