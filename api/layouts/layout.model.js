edb91ce32d75fce47b99012c5ad9bc95ce1567de
Larson <rlrson@uiowa.edu>
2018-09-04 18:10:49 -0500

    Return layout on model POST

--- api/layouts/layout.model.js
+++ api/layouts/layout.model.js
@@ -59,7 +59,10 @@ LayoutModel.postLayout = function(layout) {
   return new Promise((resolve, reject) => {
     LayoutModel.create(layout, (err, data) => {
       if (err) return reject(err);
-      else resolve(data);
+      else {
+        const dynamo_layout = data.get();
+        resolve(dynamo_layout);
+      }
     });
   });
 }
