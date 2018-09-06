13b71681970e85addb7b4d1de1509dc7760dd3b2
Larson <rlrson@uiowa.edu>
2018-09-06 17:13:14 -0500

    Echo field/value/path from layout middleware

--- api/layouts/layout.utils.js
+++ api/layouts/layout.utils.js
@@ -142,7 +142,7 @@ async function getLayoutsMiddleware(request, response, next) {
 
     next();
   } catch(err) {
-    return response.status(400).json(err);
+    return response.status(400).json({error: err, field, value, path: request.path});
   }
 }
 
