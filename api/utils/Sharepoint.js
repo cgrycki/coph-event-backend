82727c5272457038bdc21ef800352d448d32d926
Larson <rlrson@uiowa.edu>
2018-09-26 18:51:35 -0500

    Add more info to error message in SP middleware

--- api/utils/Sharepoint.js
+++ api/utils/Sharepoint.js
@@ -18,15 +18,15 @@ class Sharepoint {
       event_name: info.event_name,
       package_id: info.package_id,
       url       : `https://dev.cphb-events.public-health.uiowa.edu/cphit/${info.package_id}`
-    }
+    };
 
     return flowBody;
   }
 
   static getURI(method) {
     const methodMap = {
-      post: 'https://prod-82.westus.logic.azure.com:443/workflows/44fffe50a0a944438fd8b4affc57e2b9/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=nKZ86C_0Xq7Ito5MJYWgQHnV43img-6UHdFdRDvsMZA',
-      patch: 'https://prod-51.westus.logic.azure.com:443/workflows/ecc2d11308e446caadb5221276def81f/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=zI7OYQ-WRxYBY9xilUsiFjv4ZIbjbfe7VdSx5vT-xjk',
+      post  : 'https://prod-82.westus.logic.azure.com:443/workflows/44fffe50a0a944438fd8b4affc57e2b9/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=nKZ86C_0Xq7Ito5MJYWgQHnV43img-6UHdFdRDvsMZA',
+      patch : 'https://prod-51.westus.logic.azure.com:443/workflows/ecc2d11308e446caadb5221276def81f/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=zI7OYQ-WRxYBY9xilUsiFjv4ZIbjbfe7VdSx5vT-xjk',
       delete: 'https://prod-10.westus.logic.azure.com:443/workflows/04d626696e7a489a9ffa451da7889312/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=bZ7vy3-DIG7GBYWW8Qv-jLkki8mMi7LeyWJlAG_HQDE'
     };
     return methodMap[method];
@@ -92,11 +92,11 @@ class Sharepoint {
           break;
         default:
           return response.status(400).json({ error: true, message: 'Method not recognized'});
-      }
+      };
 
       return next();
     } catch(err) {
-      return response.status(400).json({ error: err, method });
+      return response.status(400).json({ error: err, method, result, event: request.events[0] });
     }
   }
 }
