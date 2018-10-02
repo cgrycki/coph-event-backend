3b6b3469bd3e573f0f784a103bca2d636aa2e31c
IOWA\cgrycki <christopher-grycki@uiowa.edu>
2018-10-02 13:29:34 -0500

    caught an error, more debugging

--- api/layouts/layout.utils.js
+++ api/layouts/layout.utils.js
@@ -254,6 +254,8 @@ async function getLayoutsMiddleware(request, response, next) {
         request.events  = events_with_layouts;
       } catch (zipErr) {
         console.log('got a zip error');
+        console.log(zipErr);
+        console.log(events_with_layouts);
         return response.status(400).json({ err: zipErr, layouts, events: request.events, lays });
       }
     }
